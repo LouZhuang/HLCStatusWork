fb98c8b8ff30236a85a33eaf52c45b48331182dc
huanglch <huanglch@BSTdeMacBook-Pro-5.local>
2016-08-11 12:01:06 +0800

    123

---
+++ HLCStatus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLCStatus
+//
+//  Created by huanglch on 16/8/11.
+//  Copyright © 2016年 huanglch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
